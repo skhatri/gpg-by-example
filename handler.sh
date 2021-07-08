cf908dd3443dbb487ed0785b3e3b9ef5085a5cce
Suresh Khatri <servlet2@msn.com>
2021-07-08 19:05:05 +1000

    display result

--- handler.sh
+++ handler.sh
@@ -1,29 +1,51 @@
 #!/usr/bin/env bash
-
+script_dir=$(dirname $0)
 cmd=$1
 
-if [[ ! -f /opt/app/download-${cmd}-upload.sh ]];
-then 
-  echo "command ${cmd} not understood"
-  exit 1;
-fi;
 
-shift 1;
-/opt/app/download-${cmd}-upload.sh "$@"
-exit_code=$?
-result="success"
-if [[ ${exit_code} -ne 0 ]];then 
-    result="failure"
-fi;
-echo "status: ${result}"
 
-if [[ ! -z ${WEBHOOK_URL} ]] && [[ ! -z ${WEBHOOK_NOTIFICATION_TEMPLATE} ]];
-then 
-    data=$(echo "${WEBHOOK_NOTIFICATION_TEMPLATE}"|sed "s/__DOWNLOAD_FILE__/${DOWNLOAD_FILE}/g;s/__UPLOAD_FILE__/${UPLOAD_FILE}/g;s/__STATUS__/${result}/g")
-    curl -H"Content-Type: application/json" -sk ${WEBHOOK_URL} -d ${data}
-    notification_result=$?
-    if [[ $notification_result -ne 0 ]];
+
+_preconfigure() { 
+  if [[ ! -z ${CONFIG_ENDPOINT} ]]; then 
+    curl -o /tmp/config.json -sk ${CONFIG_ENDPOINT}
+    : "${CONFIG_SELECTOR:=".data.configMap"}"
+
+  fi;
+}
+
+
+_notify() {
+    if [[ ! -z ${WEBHOOK_ENDPOINT} ]] && [[ ! -z ${WEBHOOK_NOTIFICATION_TEMPLATE} ]];
     then 
-      echo "failed to send notification."
+        data=$(echo "${WEBHOOK_NOTIFICATION_TEMPLATE}"|sed "s/__DOWNLOAD_FILE__/${DOWNLOAD_FILE}/g;s/__UPLOAD_FILE__/${UPLOAD_FILE}/g;s/__STATUS__/${result}/g")
+        curl -H"Content-Type: application/json" -sk ${WEBHOOK_ENDPOINT} -d ${data}
+        notification_result=$?
+        if [[ $notification_result -ne 0 ]];
+        then 
+        echo "failed to send notification."
+        fi;
     fi;
-fi;
+}
+
+
+_execute() {
+    if [[ ! -f ${script_dir}/download-${cmd}-upload.sh ]];
+    then 
+        echo "command ${cmd} not understood"
+        exit 1;
+    fi;
+    shift 1;
+
+    _preconfigure
+    ${script_dir}/download-${cmd}-upload.sh "$@"
+    exit_code=$?
+    result="success"
+    if [[ ${exit_code} -ne 0 ]];then 
+        result="failure"
+    fi;
+    echo "{\"result\": \"${result}\", \"operation\":\"${cmd}\", \"download_path\":\"${DOWNLOAD_FILE}\", \"upload_path\":\"${UPLOAD_FILE}\"}"
+    _notify
+}
+
+
+_execute
--- test-download-encrypt-upload.sh
+++ test-download-encrypt-upload.sh
@@ -1,4 +1,9 @@
 rm -rf ~/.gnupg
+
+if [[ -f /tmp/work-dir/_encrypted_file ]]; then 
+  rm /tmp/work-dir/_encrypted_file
+fi;
+
 export PASSPHRASE_FILE=user1/password1.txt
 export OWNER_KEY_FILE=user1/user1.key
 export OWNER_ID=user1@me.com
@@ -8,5 +13,5 @@ export PARTNER_PUBLIC_KEY_FILE=user1/user2.pub
 export DOWNLOAD_FILE=s3://gpg-by-example/test1/input.txt
 export UPLOAD_FILE=s3://gpg-by-example/test1-encrypted/input.encrypted.txt
 export S3_SECRETS_FILE=user1/secrets/S3_DATA
-./download-encrypt-upload.sh 
+./handler.sh "encrypt"
 
