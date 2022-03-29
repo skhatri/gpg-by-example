f16131255693cf0e67826062ef1ee856c2b66150
Flook Peter <pflooky@hotmail.com>
2022-03-29 11:53:27 +0800

    Put back skhatri build owner and update version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 artifact=gpg
-owner=pflookyy
-version=0.7.4
+owner=skhatri
+version=0.7.5
 docker build --no-cache -t ${owner}/${artifact}:${version} .
 docker push ${owner}/${artifact}:${version}
 
